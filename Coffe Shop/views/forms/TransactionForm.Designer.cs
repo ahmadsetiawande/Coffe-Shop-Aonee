3a75d74a83542db5fe944fcd7496ea5f37a9caf7
Aone <ahmadsetiawande30@gmailcom>
2026-02-28 10:36:39 +0700

    update peremuan 3

---
+++ Coffe Shop/views/forms/TransactionForm.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace Coffe_Shop.views.forms
+{
+    partial class TransactionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            button1 = new Button();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            button2 = new Button();
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Menu";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(12, 32);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(124, 79);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(50, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(36, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Coffe";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(526, 114);
+            label3.Name = "label3";
+            label3.Size = new Size(74, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Main Course";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(369, 114);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 8;
+            label4.Text = "Appetizer";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(203, 114);
+            label5.Name = "label5";
+            label5.Size = new Size(62, 15);
+            label5.TabIndex = 9;
+            label5.Text = "Non Coffe";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(703, 114);
+            label6.Name = "label6";
+            label6.Size = new Size(45, 15);
+            label6.TabIndex = 10;
+            label6.Text = "Dessert";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 132);
+            button1.Name = "button1";
+            button1.Size = new Size(142, 40);
+            button1.TabIndex = 11;
+            button1.Text = "Sample Menu";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(664, 32);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(124, 79);
+            pictureBox2.TabIndex = 12;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(503, 32);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(124, 79);
+            pictureBox3.TabIndex = 13;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(337, 32);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(124, 79);
+            pictureBox4.TabIndex = 14;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(174, 32);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(124, 79);
+            pictureBox5.TabIndex = 15;
+            pictureBox5.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(174, 132);
+            button2.Name = "button2";
+            button2.Size = new Size(142, 40);
+            button2.TabIndex = 16;
+            button2.Text = "Sample Menu";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(textBox6);
+            panel1.Controls.Add(textBox5);
+            panel1.Controls.Add(textBox4);
+            panel1.Controls.Add(textBox3);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(label12);
+            panel1.Controls.Add(label11);
+            panel1.Controls.Add(label10);
+            panel1.Controls.Add(label9);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(337, 132);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(451, 271);
+            panel1.TabIndex = 17;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(97, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(246, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(19, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(38, 15);
+            label7.TabIndex = 18;
+            label7.Text = "Menu";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(19, 150);
+            label8.Name = "label8";
+            label8.Size = new Size(24, 15);
+            label8.TabIndex = 19;
+            label8.Text = "Tax";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(19, 118);
+            label9.Name = "label9";
+            label9.Size = new Size(55, 15);
+            label9.TabIndex = 20;
+            label9.Text = "Sub Total";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(19, 89);
+            label10.Name = "label10";
+            label10.Size = new Size(33, 15);
+            label10.TabIndex = 21;
+            label10.Text = "Price";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(19, 57);
+            label11.Name = "label11";
+            label11.Size = new Size(53, 15);
+            label11.TabIndex = 22;
+            label11.Text = "Quantity";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(19, 179);
+            label12.Name = "label12";
+            label12.Size = new Size(32, 15);
+            label12.TabIndex = 23;
+            label12.Text = "Total";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(97, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(246, 23);
+            textBox2.TabIndex = 24;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(97, 115);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(246, 23);
+            textBox3.TabIndex = 25;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(97, 147);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(246, 23);
+            textBox4.TabIndex = 26;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(97, 86);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(246, 23);
+            textBox5.TabIndex = 27;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(97, 179);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(246, 23);
+            textBox6.TabIndex = 28;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(201, 219);
+            button3.Name = "button3";
+            button3.Size = new Size(142, 40);
+            button3.TabIndex = 18;
+            button3.Text = "Sample Menu";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(19, 219);
+            button4.Name = "button4";
+            button4.Size = new Size(142, 40);
+            button4.TabIndex = 19;
+            button4.Text = "Sample Menu";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // TransactionForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(button2);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(button1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
+            Controls.Add(label1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "TransactionForm";
+            Text = "TransactionForm";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private Button button2;
+        private Panel panel1;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private Label label9;
+        private Label label8;
+        private Label label7;
+        private TextBox textBox1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox6;
+    }
+}
